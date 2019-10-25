b6d1bd4499b860f6055b66477bd62ec664910cbb
Brandon Brasson <rocksong622@gmail.com>
2019-10-25 07:18:56 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -34,7 +34,7 @@ class Student
       sql = <<-SQL
       INSERT INTO students (name, grade)
 
-    VALUES name
+    VALUES (?, ?)
       SQL
       DB[:conn].execute(sql, self.name, self.grade)
       @id = DB[:conn].execute("SELECT * FROM students")[0][0]
