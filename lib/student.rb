6ae74f862fe61c09f79acd85930a0f12043f8c55
Brandon Brasson <rocksong622@gmail.com>
2019-10-25 06:33:17 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,6 +1,26 @@
 class Student
 
   # Remember, you can access your database connection anywhere in this class
-  #  with DB[:conn]  
-  
+  #  with DB[:conn]
+
+  attr_accessor :name, :album, :id
+
+  def initialize(name, id=nil)
+    @id = id
+    @name = name
+    @album = album
+  end
+
+  def self.create_table
+    sql =  <<-SQL
+      CREATE TABLE IF NOT EXISTS songs (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        album TEXT
+        )
+        SQL
+    DB[:conn].execute(sql)
+  end
+
+end
 end
