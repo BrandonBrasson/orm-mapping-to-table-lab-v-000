a3318031d9176f526b5560bf31a7e9facd5901db
Brandon Brasson <rocksong622@gmail.com>
2019-10-25 06:58:18 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -29,8 +29,7 @@ class Student
     def save
       sql = <<-SQL
       INSERT INTO students (name, grade)
-
-    VALUES true || false
+      VALUES true || false
       SQL
 
       DB[:conn].execute(sql, self.name, self.grade)
