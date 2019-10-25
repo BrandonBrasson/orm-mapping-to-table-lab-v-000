e24fac91b5251ce576c32f1a38b37657106a6e86
Brandon Brasson <rocksong622@gmail.com>
2019-10-25 06:38:26 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,23 +1,21 @@
-class Student
+lass Student
+  attr_accessor :name, :grade
+  attr_reader :id
 
-  # Remember, you can access your database connection anywhere in this class
-  #  with DB[:conn]
-
-  attr_accessor :name,  :id
-
-  def initialize(name, id=nil)
+  def initialize(name, grade, id=nil)
     @id = id
     @name = name
+    @grade = grade
   end
 
   def self.create_table
     sql =  <<-SQL
-      CREATE TABLE IF NOT EXISTS songs (
+      CREATE TABLE IF NOT EXISTS students (
         id INTEGER PRIMARY KEY,
         name TEXT,
-
+        grade TEXT
         )
-        SQL
+    SQL
     DB[:conn].execute(sql)
   end
 
